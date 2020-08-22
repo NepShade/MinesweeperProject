f197465923c80d8811fc13635ee071e50b6fd86c
NepShade <NocturneNep@gmail.com>
2020-08-22 23:47:44 +0200

    Added TraditionalMinefieldCreator.cs and HardcoreMinefieldCreator.cs

---
+++ MinesweeperProject/MinesweeperLibrary/HardcoreMinefieldCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinesweeperLibrary
+{
+    // Classe rappresentante un creatore di campi minati 'hardcore'
+    public class HardcoreMinefieldCreator : IMinefieldCreator
+    {
+        // Metodo che restituisce un'istanza di un campo minato 'hardcore'
+        public Minefield CreateMinefield(int length, int height, int mines)
+        {
+            return new HardcoreMinefield(length, height, mines);
+        }
+    }
+}
---
+++ MinesweeperProject/MinesweeperLibrary/TraditionalMinefieldCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinesweeperLibrary
+{
+    // Classe rappresentante un creatore di campi minati 'tradizionali'
+    public class TraditionalMinefieldCreator : IMinefieldCreator
+    {
+        // Metodo che restituisce un'istanza di un campo minato 'tradizionale'
+        public Minefield CreateMinefield(int length, int height, int mines)
+        {
+            return new TraditionalMinefield(length, height, mines);
+        }
+    }
+}
