d9ef8ad15899ecb39520485eb09fa03afe3e5bb5
NepShade <NocturneNep@gmail.com>
2020-08-22 23:45:23 +0200

    Added IMinefieldCreator.cs

---
+++ MinesweeperProject/MinesweeperLibrary/IMinefieldCreator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinesweeperLibrary
+{
+    // Interfaccia di un creatore di campi minati
+    public interface IMinefieldCreator
+    {
+        // Metodo che restituisce un'istanza di un campo minato
+        Minefield CreateMinefield(int length, int height, int mines);
+    }
+}
