9498f51662c1a41cb704abf1f88f167573a4edf3
NepShade <NocturneNep@gmail.com>
2020-08-23 23:34:42 +0200

    Added MainView.cs

---
+++ MinesweeperProject/MinesweeperGUI/MainView.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace MinesweeperGUI
+{
+    partial class MainView
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this._gameButton = new System.Windows.Forms.Button();
+            this._minesCounterLabel = new System.Windows.Forms.Label();
+            this._minesweeperMenuStrip = new System.Windows.Forms.MenuStrip();
+            this._gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._rulesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._gameDurationLabel = new System.Windows.Forms.Label();
+            this._gameTimer = new System.Windows.Forms.Timer(this.components);
+            this._minesweeperMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _gameButton
+            // 
+            this._gameButton.Location = new System.Drawing.Point(67, 30);
+            this._gameButton.Name = "_gameButton";
+            this._gameButton.Size = new System.Drawing.Size(35, 35);
+            this._gameButton.TabIndex = 0;
+            this._gameButton.UseVisualStyleBackColor = true;
+            // 
+            // _minesCounterLabel
+            // 
+            this._minesCounterLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this._minesCounterLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._minesCounterLabel.Location = new System.Drawing.Point(10, 35);
+            this._minesCounterLabel.Name = "_minesCounterLabel";
+            this._minesCounterLabel.Size = new System.Drawing.Size(50, 25);
+            this._minesCounterLabel.TabIndex = 1;
+            this._minesCounterLabel.Text = "????";
+            this._minesCounterLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // _minesweeperMenuStrip
+            // 
+            this._minesweeperMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._gameToolStripMenuItem,
+            this._infoToolStripMenuItem});
+            this._minesweeperMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this._minesweeperMenuStrip.Name = "_minesweeperMenuStrip";
+            this._minesweeperMenuStrip.Size = new System.Drawing.Size(180, 24);
+            this._minesweeperMenuStrip.TabIndex = 2;
+            // 
+            // _gameToolStripMenuItem
+            // 
+            this._gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._newToolStripMenuItem,
+            this._settingsToolStripMenuItem,
+            this._rulesToolStripMenuItem,
+            this._exitToolStripMenuItem});
+            this._gameToolStripMenuItem.Name = "_gameToolStripMenuItem";
+            this._gameToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this._gameToolStripMenuItem.Text = "Partita";
+            // 
+            // _newToolStripMenuItem
+            // 
+            this._newToolStripMenuItem.Name = "_newToolStripMenuItem";
+            this._newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this._newToolStripMenuItem.Text = "Nuova";
+            // 
+            // _settingsToolStripMenuItem
+            // 
+            this._settingsToolStripMenuItem.Name = "_settingsToolStripMenuItem";
+            this._settingsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this._settingsToolStripMenuItem.Text = "Configura";
+            // 
+            // _rulesToolStripMenuItem
+            // 
+            this._rulesToolStripMenuItem.Name = "_rulesToolStripMenuItem";
+            this._rulesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this._rulesToolStripMenuItem.Text = "Regole";
+            // 
+            // _exitToolStripMenuItem
+            // 
+            this._exitToolStripMenuItem.Name = "_exitToolStripMenuItem";
+            this._exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this._exitToolStripMenuItem.Text = "Esci";
+            // 
+            // _infoToolStripMenuItem
+            // 
+            this._infoToolStripMenuItem.Name = "_infoToolStripMenuItem";
+            this._infoToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this._infoToolStripMenuItem.Text = "Info";
+            // 
+            // _gameDurationLabel
+            // 
+            this._gameDurationLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this._gameDurationLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._gameDurationLabel.Location = new System.Drawing.Point(110, 35);
+            this._gameDurationLabel.Name = "_gameDurationLabel";
+            this._gameDurationLabel.Size = new System.Drawing.Size(60, 25);
+            this._gameDurationLabel.TabIndex = 3;
+            this._gameDurationLabel.Text = "??:??";
+            this._gameDurationLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // _gameTimer
+            // 
+            this._gameTimer.Interval = 1000;
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(180, 240);
+            this.Controls.Add(this._gameDurationLabel);
+            this.Controls.Add(this._minesCounterLabel);
+            this.Controls.Add(this._gameButton);
+            this.Controls.Add(this._minesweeperMenuStrip);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this._minesweeperMenuStrip;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainView";
+            this.Text = "Campo Minato";
+            this._minesweeperMenuStrip.ResumeLayout(false);
+            this._minesweeperMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _gameButton;
+        private System.Windows.Forms.Label _minesCounterLabel;
+        private System.Windows.Forms.MenuStrip _minesweeperMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem _gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _rulesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _exitToolStripMenuItem;
+        private System.Windows.Forms.Label _gameDurationLabel;
+        private System.Windows.Forms.Timer _gameTimer;
+        private System.Windows.Forms.ToolStripMenuItem _infoToolStripMenuItem;
+    }
+}
+
---
+++ MinesweeperProject/MinesweeperGUI/MainView.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinesweeperGUI
+{
+    // Classe rappresentante il form principale di gioco
+    public partial class MainView : Form
+    {
+        // Attributi della classe
+        // spaziatura dal bordo superiore del form
+        public const int TopEdgeSpacing = 70;
+        // spaziatura dal bordo inferiore e dai bordi laterali del form
+        public const int EdgeSpacing = 10;
+        // altezza occupata dal menu principale di gioco nel form
+        private const int MainMenuHeight = 25;
+
+        // Costruttore che inizializza i componenti del form
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        // Metodo che ridimensiona l'area client del form e che ricolloca al suo interno alcuni elementi
+        public void ResizeClientArea(int newLength, int newHeight)
+        {
+            // viene ridimensionata l'area client del form
+            int lengthClientArea = EdgeSpacing + newLength + EdgeSpacing;
+            int heightClientArea = TopEdgeSpacing + newHeight + EdgeSpacing;
+            ClientSize = new Size(lengthClientArea, heightClientArea);
+
+            // si calcola lo spazio occupano dal 'menu della partita'
+            int GameMenuHeight = TopEdgeSpacing - MainMenuHeight;
+
+            // si cambia la posizione dell'etichetta-timer all'interno del form
+            int beginTimerX = lengthClientArea - EdgeSpacing - _gameDurationLabel.Width;
+            int beginTimerY = ((GameMenuHeight - _gameDurationLabel.Height) / 2) + MainMenuHeight;
+            _gameDurationLabel.Location = new Point(beginTimerX, beginTimerY);
+
+            // si calcola la coordinata X del punto in cui l'etichetta-contatore finisce nel form
+            int endCounterX = _minesCounterLabel.Location.X + _minesCounterLabel.Width;
+            // si calcola la distanza tra l'etichetta-contatore e l'etichetta-timer
+            int spaceBetweenTimerAndCounter = beginTimerX - endCounterX;
+            // si calcola e si arrotonda lo scostamento del bottone di gioco dal contatore e dal timer
+            double spaceFromTimerAndCounter = (spaceBetweenTimerAndCounter - _gameButton.Width) / 2;
+            int roundedSpaceFromTimerAndCounter = (int)Math.Round(spaceFromTimerAndCounter);
+
+            // si cambia la posizione del bottone di gioco all'interno del form
+            int beginButtonX = endCounterX + roundedSpaceFromTimerAndCounter;
+            int beginButtonY = ((GameMenuHeight - _gameButton.Height) / 2) + MainMenuHeight;
+            _gameButton.Location = new Point(beginButtonX, beginButtonY);
+        }
+    }
+}
